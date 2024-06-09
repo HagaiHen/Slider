c82b870d22cad54636ca9a51b8cff2987688eabe
hagai_hen <hagai349@gmail.com>
2024-06-09 20:25:56 +0300

    updated readme

--- src/slider.js
+++ src/slider.js
@@ -2,20 +2,23 @@ import React, { useState, useEffect } from 'react';
 import useSlide from './useSlide';
 import './slider.css';
 
+// Define slides data
 const slides = [
     { id: 1, content: 'Slide 1' },
     { id: 2, content: 'Slide 2' },
     { id: 3, content: 'Slide 3' },
   ];
 
-
+// Slider component
 const Slider = () => {
 
   const [currentSlide, setCurrentSlide] = useState(0);
   const [isAutoScroll, setIsAutoScroll] = useState(false);
   
+  // Custom hook for sliding
   const { nextSlide, prevSlide, startAutoScroll, stopAutoScroll } = useSlide(slides.length, setCurrentSlide);
 
+  // Effect hook to start/stop auto scroll based on isAutoScroll state
   useEffect(() => {
     if (isAutoScroll) {
       startAutoScroll();
--- src/useSlide.js
+++ src/useSlide.js
@@ -1,25 +1,30 @@
 import { useRef } from 'react';
 
+
+// Custom hook for handling slide functionalities
 const useSlide = (totalSlides, setCurrentSlide) => {
-  const intervalRef = useRef(null);
 
-  const nextSlide = () => {
-    setCurrentSlide((prev) => (prev + 1) % totalSlides);
-  };
+    // Refrence to hold the interval for auto-scrolling
+    const intervalRef = useRef(null);
 
-  const prevSlide = () => {
-    setCurrentSlide((prev) => (prev - 1 + totalSlides) % totalSlides);
-  };
+    const nextSlide = () => {
+        setCurrentSlide((prev) => (prev + 1) % totalSlides);
+    };
 
-  const startAutoScroll = () => {
-    if (intervalRef.current) return;
-    intervalRef.current = setInterval(nextSlide, 2000);
+    const prevSlide = () => {
+        setCurrentSlide((prev) => (prev - 1 + totalSlides) % totalSlides);
+    };
+
+    const startAutoScroll = () => {
+        if (intervalRef.current) return;
+        // Run the function every 2000ms (2 seconds)
+        intervalRef.current = setInterval(nextSlide, 2000);
   };
 
   const stopAutoScroll = () => {
     if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
     }
   };
 
